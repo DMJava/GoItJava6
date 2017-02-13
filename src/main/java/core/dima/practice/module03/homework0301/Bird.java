7f02e0940bf2ea409e77697117a530e260ecf821
DmytroM <avangardi@ukr.net>
2017-02-13 19:19:16 +0200

    module3ispravil

--- src/main/java/core/dima/practice/module03/homework0301/Bird.java
+++ src/main/java/core/dima/practice/module03/homework0301/Bird.java
@@ -2,13 +2,15 @@ package core.dima.practice.module03.homework0301;
 
 public class Bird {
     public static void main(String[] args) {
-        sing("I am flying");
-        sing("I am walking");
-        sing("I am singing");
-        sing("I am Bird");
+
+        Bird bird = new Bird();
+        bird.sing();
     }
 
-    public static void sing(String s) {
-        System.out.println(s);
+    public void sing() {
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
     }
 }
