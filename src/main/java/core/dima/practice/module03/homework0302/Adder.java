eb0c105a3ac4136790116cbaf7dee2d2f2252827
DmytroM <avangardi@ukr.net>
2017-02-13 19:27:03 +0200

    module3ispravil

--- src/main/java/core/dima/practice/module03/homework0302/Adder.java
+++ src/main/java/core/dima/practice/module03/homework0302/Adder.java
@@ -3,11 +3,8 @@ package core.dima.practice.module03.homework0302;
 import core.dima.practice.module03.homework0302.Arithmetic;
 
 public class Adder extends Arithmetic {
-    boolean check(Integer a, Integer b) {
-        if (a >= b) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean check(Integer a, Integer b) {
+        return a >= b;
+
     }
 }
--- src/main/java/core/dima/practice/module03/homework0302/Arithmetic.java
+++ src/main/java/core/dima/practice/module03/homework0302/Arithmetic.java
@@ -2,7 +2,6 @@ package core.dima.practice.module03.homework0302;
 
 public class Arithmetic {
     public int add(Integer a, Integer b) {
-        Integer c = a + b;
-        return c;
+        return a + b;
     }
 }
