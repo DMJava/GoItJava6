66f595586634de1c00d76aaf1584494dcd894d0c
DmytroM <avangardi@ukr.net>
2017-03-08 12:48:55 +0200

    module06_test

--- src/main/java/core/dima/practice/module06/task0604/CitizenMain.java
+++ src/main/java/core/dima/practice/module06/task0604/CitizenMain.java
@@ -2,11 +2,13 @@ package core.dima.practice.module06.task0604;
 
 public class CitizenMain {
     public static void main(String[] args) {
-        sayHello(new CitizenFactory.Chinese());
         sayHello(new CitizenFactory.Englishman());
+        sayHello(new CitizenFactory.Chinese());
+        sayHello(new CitizenFactory.Russian());
+        sayHello(new CitizenFactory.Italian());
     }
 
     private static void sayHello(Citizen citizen) {
-        System.out.println(citizen.getClass().getSimpleName());
+        System.out.println(citizen.sayHello());
     }
 }
